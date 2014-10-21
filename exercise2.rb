521c482e57788678bb352291479cb3655ee7093c
wduntak <wduntak@gmail.com>
2014-10-21 10:53:13 -0400

    Using string interpolation

--- exercise2.rb
+++ exercise2.rb
@@ -8,3 +8,11 @@ puts meal_with_tip
 num = 2
 add = "This costs $" + num.to_s + "."
 puts add
+
+#Placing integer result with string interpolation
+num1 = 45628
+num2 = 7839
+
+result = num1 * num2
+
+puts "The result of #{num1} * #{num2} = #{result}"
