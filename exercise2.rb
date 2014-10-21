b1f7df64988c45585fe380cce2eae2a563d4fe70
wduntak <wduntak@gmail.com>
2014-10-21 11:54:14 -0400

    Created options for tips on exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,8 +1,25 @@
 # Calculate 20% tip for $55 meal
 meal_cost = 55
-meal_with_tip = (meal_cost * 0.20) + meal_cost
 
-puts meal_with_tip
+puts "How much tip would you like to give? (A-10%, B-15%, C-20%)"
+
+tip = gets.chomp.downcase
+
+if tip == "a"
+	meal_with_tip = (meal_cost * 0.10) + meal_cost
+	puts "The total is #{meal_with_tip}."
+
+elsif tip == "b"
+	meal_with_tip = (meal_cost * 0.15) + meal_cost
+	puts "The total is #{meal_with_tip}"
+
+elsif tip == "c"
+	meal_with_tip = (meal_cost * 0.20) + meal_cost
+	puts "The total is #{meal_with_tip}"
+
+else
+	puts "I'm sorry that is not an available option."
+end
 
 # Add string and integer
 num = 2
