bc504830ee36557cc39d30ff4c543295566bb439
wduntak <wduntak@gmail.com>
2014-10-21 10:01:06 -0400

    Adding a  string and integer

--- exercise2.rb
+++ exercise2.rb
@@ -1,4 +1,10 @@
+# Calculate 20% tip for $55 meal
 meal_cost = 55
 meal_with_tip = (meal_cost * 0.20) + meal_cost
 
 puts meal_with_tip
+
+# Add string and integer
+num = 2
+add = "This costs $" + num.to_s + "."
+puts add
