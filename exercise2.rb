ce40ce042deea74dd498619e281affd012e253c8
wduntak <wduntak@gmail.com>
2014-10-21 12:09:30 -0400

    Provide a loop to re-enter tip option

--- exercise2.rb
+++ exercise2.rb
@@ -1,6 +1,10 @@
 # Calculate 20% tip for $55 meal
 meal_cost = 55
 
+isValid = false
+
+until isValid
+
 puts "How much tip would you like to give? (A-10%, B-15%, C-20%)"
 
 tip = gets.chomp.downcase
@@ -8,17 +12,19 @@ tip = gets.chomp.downcase
 if tip == "a"
 	meal_with_tip = (meal_cost * 0.10) + meal_cost
 	puts "The total is #{meal_with_tip}."
-
+	isValid = true
 elsif tip == "b"
 	meal_with_tip = (meal_cost * 0.15) + meal_cost
 	puts "The total is #{meal_with_tip}"
-
+	isValid = true
 elsif tip == "c"
 	meal_with_tip = (meal_cost * 0.20) + meal_cost
 	puts "The total is #{meal_with_tip}"
-
+	isValid = true
 else
 	puts "I'm sorry that is not an available option."
+	isValid = false
+end
 end
 
 # Add string and integer
